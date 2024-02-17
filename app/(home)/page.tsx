3a1fad14981a2e0a32202c6271f1bdfc88b589ce
jin123457 <jin_ox@naver.com>
2024-02-17 14:43:07 +0900

    parallel request & create loading page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,27 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-const url = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(url);
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
 
 export default async function HomePage() {
   const moives = await getMovies();
-  return <p>{JSON.stringify(moives)}</p>;
+  return (
+    <ul>
+      {moives.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <h2>movie datail Loading ....</h2>;
+}
