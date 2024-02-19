b17219b1731d9cd5a5c5d91eb509bfb5b8e500a5
jin123457 <jin_ox@naver.com>
2024-02-19 16:09:50 +0900

    learn to Suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,20 +1,23 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id: string) {
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const [movie, video] = await Promise.all([getMovie(id), getVideos(id)]);
-  return <h1>{movie.title}</h1>;
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={`Loading movie info`}>
+        {/* @ts-expect-error Async Server Component */}
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={`Loading movie video`}>
+        {/* @ts-expect-error Async Server Component */}
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
