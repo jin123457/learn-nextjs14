fc4f672b6dc8b703c643b81bd16c5bfab38472db
jin123457 <jin_ox@naver.com>
2024-02-20 11:04:06 +0900

    update typescript

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -17,11 +17,9 @@ export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={`Loading movie info`}>
-        {/* @ts-expect-error Async Server Component */}
         <MovieInfo id={id} />
       </Suspense>
       <Suspense fallback={`Loading movie video`}>
-        {/* @ts-expect-error Async Server Component */}
         <MovieVideos id={id} />
       </Suspense>
     </div>
